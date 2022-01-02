f953a6367616210b6e2be2c081d0505c4f4a37b1
J. Nick Koston <nick@koston.org>
2022-01-02 16:41:53 -0600

    add example

---
+++ example.py
@@ -0,0 +1,20 @@
+import asyncio
+import pprint
+
+import aiohttp
+from aiosteamist import Steamist
+
+
+async def main():
+    websession = aiohttp.ClientSession()
+    steamist = Steamist("192.168.210.105", websession)
+    devices = await steamist.async_get_status()
+    import pprint
+
+    pprint.pprint(devices)
+    await websession.close()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
