058ecd16440e4dca41c60cbe07075ce8bd767358
J. Nick Koston <nick@koston.org>
2022-06-25 00:29:18 -0700

    Bump version: 0.3.1 → 0.3.2

--- aiosteamist/__init__.py
+++ aiosteamist/__init__.py
@@ -9,7 +9,7 @@ import xmltodict  # type: ignore
 
 __author__ = """J. Nick Koston"""
 __email__ = "nick@koston.org"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 DEFAULT_REQUEST_TIMEOUT = 10
 STATUS_ENDPOINT = "/status.xml"
